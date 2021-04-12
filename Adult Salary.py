532fdc92f1d646480821ff20eb1f22183d6b05d6
Chirag Verma <cv091998@gmail.com>
2021-04-12 14:13:46 +0530

    Update Adult Salary.py

--- Adult Salary.py
+++ Adult Salary.py
@@ -20,7 +20,7 @@ dataset = pd.read_csv('Adult Sal.csv', names = ['age',
                                           'salary'],
                         na_values = ' ?')
 
-dataste.isnull()
+dataset.isnull()
 
 X = dataset.iloc[:, 0:14].values
 y = dataset.iloc[:, -1].values
